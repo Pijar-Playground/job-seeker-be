33a0e1a4f5e56a37ca211387d98af523ad1f14dd
Bilkis Ismail <bilkisismail07@gmail.com>
2023-02-09 13:36:18 +0700

    [FIX] Limit api

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.use(
 );
 
 const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 15 minutes
-  max: 150, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  max: 200, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
